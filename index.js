b2d4c32721b0fad1dc086505d4453e966593dd2b
tothpeter1216 <tothpeter1216@gmail.com>
2020-12-30 12:50:55 +0100

    mongoose connection works with dotenv

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
 const app = require("express")();
-const PORT = 3001;
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const url = process.env.URL;
+
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
+
+const schoolSchema = new mongoose.Schema({
+  name: String,
+  link: String,
+  description: String,
+});
+
+const School = mongoose.model("School", schoolSchema);
+
+const school1 = new School({
+  name: "gf",
+  link: "www.ize",
+  description: "cooltour",
+});
+
+school1.save().then((result) => {
+  console.log("school saved!");
+  mongoose.connection.close();
+});
 
 app.get("/", (req, res) => {
   res.send("HIII");
 });
 
-app.listen(PORT, () => {
-  console.log("The server is runing on " + PORT);
+app.listen(process.env.PORT, () => {
+  console.log("The server is runing on " + process.env.PORT);
 });
